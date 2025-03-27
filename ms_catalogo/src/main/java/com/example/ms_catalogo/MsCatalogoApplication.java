7a2de5bf71256974d9f24e627bbe7c0810ee5203
yurinscoaquira <119636078+yurinscoaquira@users.noreply.github.com>
2025-03-27 10:50:48 -0500

    primer
    primer

---
+++ ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ms_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCatalogoApplication.class, args);
+    }
+
+
+}
