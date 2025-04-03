325fd4426a8c12a66674f713b6c7d7e43d662f44
Yurins <mijhaelyurinscoaquirajusto@gmail.com>
2025-04-03 08:47:38 -0500

    igualandome al profe

--- ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
+++ ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsCatalogoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogoApplication.class, args);
     }
-
 
 }
