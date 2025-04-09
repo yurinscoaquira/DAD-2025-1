58f42ddfabb4412df57ad89f3ef496e44baa0b98
Yurins <mijhaelyurinscoaquirajusto@gmail.com>
2025-04-09 18:11:05 -0500

    mi primer commit

---
+++ ms-gateway-service/src/test/java/com/example/msgatewayservice/MsGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
